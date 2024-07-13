30f45037c0e376a2f14a174e5c00cde687ecd92e
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2024-07-13 23:46:21 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,319 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<>
+			<section
+				id="Projects"
+				className="w-fit mx-auto grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5 py-10"
+			>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+				<div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+					<Link href="/product">
+						<img
+							src="https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+							alt="Product"
+							className="h-80 w-72 object-cover rounded-t-xl"
+						/>
+						<div className="px-4 py-3 w-72">
+							<span className="text-gray-400 mr-3 uppercase text-xs">
+								Brand
+							</span>
+							<p className="text-lg font-bold text-black truncate block capitalize">
+								Product Name
+							</p>
+							<div className="flex items-center">
+								<p className="text-lg font-semibold text-black cursor-auto my-3">
+									$149
+								</p>
+								<del>
+									<p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+								</del>
+							</div>
+						</div>
+					</Link>
+				</div>
+			</section>
+		</>
+	);
 }
---
+++ components/Navbar.js
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<div className="sticky top-0 bg-gray-100 z-20">
+			<header className="text-gray-600 body-font">
+				<div className="container mx-auto flex p-5 flex-row  items-center justify-between">
+					<Link
+						href={"/"}
+						className="flex title-font font-medium items-center text-gray-900 "
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							stroke="currentColor"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+							viewBox="0 0 24 24"
+						>
+							<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+						</svg>
+						<span className="ml-3 text-xl">LOGO</span>
+					</Link>
+					<Link
+						href={"/about"}
+						className="inline-flex items-center bg-gray-100 border-0 py-2 px-3 focus:outline-none hover:bg-gray-200 rounded-lg text-base "
+					>
+						About Us
+						<svg
+							fill="none"
+							stroke="currentColor"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							className="w-4 h-4 ml-1"
+							viewBox="0 0 24 24"
+						>
+							<path d="M5 12h14M12 5l7 7-7 7"></path>
+						</svg>
+					</Link>
+				</div>
+			</header>
+		</div>
+	);
+};
+
+export default Navbar;
